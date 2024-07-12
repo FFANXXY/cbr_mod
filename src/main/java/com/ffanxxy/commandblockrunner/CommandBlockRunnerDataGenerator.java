abdbacb83ae1061f1a4fa6af42725ee98eaf439c
Fanxy <yzh20110211@outlook.com>
2024-07-12 14:53:54 +0800

    Revert "t1"
    This reverts commit 8e03a5e9ec629ac1ba8efecb0fb5c89f4adb2dcd.

---
+++ src/main/java/com/ffanxxy/commandblockrunner/CommandBlockRunnerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ffanxxy.commandblockrunner;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CommandBlockRunnerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
